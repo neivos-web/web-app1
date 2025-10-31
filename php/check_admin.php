eea0df95301c24a201e9342e30658da804023aa0
SaoussenMn <saoussenmn@gmail.com>
2025-10-31 20:07:09 +0100

    cors

--- php/check_admin.php
+++ php/check_admin.php
@@ -1,4 +1,9 @@
 <?php
+
+header("Access-Control-Allow-Origin: *"); // Replace * with your domain in production
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json");
 session_start();
 header('Content-Type: application/json');
 echo json_encode(['logged_in' => isset($_SESSION['admin']) && $_SESSION['admin'] === true]);
