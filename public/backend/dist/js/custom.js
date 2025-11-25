f3d90cb25550360e9508ff305388dd4098d63ba1
boukharilina <linaboukhari09@gmail.com>
2025-11-25 23:09:36 +0100

    new_config

---
+++ public/backend/dist/js/custom.js
@@ -0,0 +1,177 @@
+/*
+*
+* Template Name: Minton - Responsive Bootstrap 4 Admin Dashboard
+* Author: Firstname Lastname
+* File: Custom Js
+*
+
+# Color Scheme
+# Sweet Alert 2
+# Toastify
+# Select2
+# DataTables
+# Dropzone
+# Summernote
+# Datetimepicker
+# Chart.js
+# CkEditor
+# Dropify
+# Nestable
+# Tippy
+# Clipboard
+# Select2
+
+
+*/
+
+// ============ DataTables  & Plugins ============
+
+$(function () {
+    // 1st Data Table
+    $("#example1")
+        .DataTable({
+            responsive: true,
+            lengthChange: false,
+            autoWidth: false,
+            buttons: ["copy", "csv", "excel", "pdf", "print", "colvis"],
+        })
+        .buttons()
+        .container()
+        .appendTo("#example1_wrapper .col-md-6:eq(0)");
+
+    // 2nd Data Table
+    $("#example2").DataTable({
+        paging: true,
+        lengthChange: true,
+        searching: true,
+        ordering: true,
+        info: true,
+        autoWidth: true,
+        responsive: true,
+    });
+});
+
+// ================= Sweet Alert 2  ==============
+
+// Data Insert Message
+
+// Data Delete Message with Alert box into get url
+
+// ===================
+
+// =================  End of Sweet Alert 2 ==============
+
+
+
+// ================= Toaster Notification Flash Message  ==============
+
+if (Session.has("success") || Session.has("error") || session.has("message")) {
+    toastr.options = {
+        closeButton: true,
+        debug: false,
+        progressBar: true,
+        positionClass: "toast-top-right",
+        showDuration: "300",
+        hideDuration: "1000",
+        timeOut: "5000",
+        extendedTimeOut: "1000",
+        showEasing: "swing",
+        hideEasing: "linear",
+        showMethod: "fadeIn",
+        hideMethod: "fadeOut",
+    };
+}
+
+// <!-- Toastify -->
+
+// =============  Start Toaster Flash Message ==================
+
+// if (Session.has("success")) {
+//     toastr.success("{{ Session::get('success') }}", "success", {
+//         timeOut: 3000,
+//         progressBar: true,
+//         closeButton: true,
+//         positionClass: "toast-top-right",
+//         hideDuration: "1000",
+//     });
+// } else if (Session.has("error")) {
+//     toastr.error("{{ Session::get('error') }}", "error", {
+//         timeOut: 2000,
+//         progressBar: true,
+//         closeButton: true,
+//         positionClass: "toast-top-right",
+//         hideDuration: "1000",
+//     });
+// }
+
+// # Switch case for all alert type
+
+if (session.has("message")) {
+    let type = "{{ Session::get('alert-type') }}";
+    switch (type) {
+        case "success":
+            // toastr.success("{{ Session::get('message') }}");
+            toastr.success(
+                "{{ Session::get('message') }}",
+                "{{ Session::get('data') }}",
+                "{{ Session::get('alert-type') }}",
+                {
+                    timeOut: 2000,
+                    progressBar: true,
+                    closeButton: true,
+                    positionClass: "toast-top-right",
+                    hideDuration: "1000",
+                }
+            );
+            break;
+
+        case "error":
+            toastr.error(
+                "{{ Session::get('message') }}",
+                "{{ Session::get('data') }}",
+                "{{ Session::get('alert-type') }}",
+                {
+                    timeOut: 2000,
+                    progressBar: true,
+                    closeButton: true,
+                    positionClass: "toast-top-right",
+                    hideDuration: "1000",
+                }
+            );
+            break;
+
+        case "warning":
+            toastr.warning("{{ Session::get('message') }}");
+            break;
+
+        case "info":
+            toastr.info("{{ Session::get('message') }}");
+            break;
+    }
+}
+
+// Sweet Alert Dailog Box
+Swal.fire({
+    title: "Congratulations",
+    text: "{{ Session::get('message') }}",
+    icon: "success",
+    confirmButtonText: "Ok",
+    confirmButtonColor: "#3085d6",
+    showCloseButton: true,
+});
+
+// =============  End of Toaster Flash Message ==================
+
+// =============  Start of Toastify js Flash Message ==================
+
+// =============  End of Toastify js Flash Message ==================
+
+
+Swal.fire({
+    title: "Congratulations",
+    text: "{{ Session::get('message') }}",
+    icon: "success",
+    confirmButtonText: "Ok",
+    confirmButtonColor: "#3085d6",
+    showCloseButton: true,
+});
